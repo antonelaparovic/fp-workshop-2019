15763bcf50c827a69baf42705a73cc270ac632b7
antonela <57138968+antonelaparovic@users.noreply.github.com>
2021-01-13 11:19:29 +0100

    Exercises (#1)
    * Do all exercises
    
    * Do all exercises
    
    * Remove unnecessary value
    
    * Define constant outside method
    
    * Fix PasswordChecker
    
    * Fix constant values

--- src/main/scala/io/lambdaworks/workshop/exceptions/PasswordChecker.scala
+++ src/main/scala/io/lambdaworks/workshop/exceptions/PasswordChecker.scala
@@ -1,16 +1,53 @@
 package io.lambdaworks.workshop.exceptions
 
+import scala.util.matching.Regex
+
 object PasswordChecker {
 
-  def validate(password: String): Either[List[Throwable], String] = ???
+  val NumberPattern: Regex = "[0-9]".r
+  val UpperPattern: Regex  = "[A-Z]".r
+  val LowerPattern: Regex  = "[a-z]".r
+  val MinimalLength: Int   = 5;
 
-  private def minNumberOfChars(password: String, length: Int): Either[Throwable, String] = ???
+  def validate(password: String): Either[List[Throwable], String] = {
 
-  private def containsUpperCase(password: String): Either[Throwable, String] = ???
+    val validators =
+      List(minNumberOfChars(password, MinimalLength),
+           containsLowerCase(password),
+           containsUpperCase(password),
+           containsNumber(password))
 
-  private def containsLowerCase(password: String): Either[Throwable, String] = ???
+    val res: Either[List[Throwable], String] = Left(validators.collect { case Left(x) => x })
 
-  private def containsNumber(password: String): Either[Throwable, String] = ???
+    if (validators.forall(_.isRight)) Right(password)
+    else res
+  }
+
+  private def minNumberOfChars(password: String, length: Int): Either[Throwable, String] = {
+    if (password.length() < length) Left(InvalidLength)
+    else Right(password)
+  }
+
+  private def containsUpperCase(password: String): Either[Throwable, String] = {
+    UpperPattern.findFirstMatchIn(password) match {
+      case Some(_) => Right(password)
+      case None    => Left(MissingUppercase)
+    }
+  }
+
+  private def containsLowerCase(password: String): Either[Throwable, String] = {
+    LowerPattern.findFirstMatchIn(password) match {
+      case Some(_) => Right(password)
+      case None    => Left(MissingLowercase)
+    }
+  }
+
+  private def containsNumber(password: String): Either[Throwable, String] = {
+    NumberPattern.findFirstMatchIn(password) match {
+      case Some(_) => Right(password)
+      case None    => Left(MissingNumber)
+    }
+  }
 
   object InvalidLength    extends Throwable("Password must contain at least 5 characters.")
   object MissingUppercase extends Throwable("Password must contain uppercase letter.")
